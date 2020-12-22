115dddfdaaf637941cd01e522012104fa9a442e3
한정훈 <onooff@naver.com>
2020-12-22 18:03:52 +0900

    백준2805 나무 자르기

--- boj/bj2805.py
+++ boj/bj2805.py
@@ -1,6 +1,26 @@
-'''
-첫줄에 N,M 초기연료
-다음부터 NM Map
-운전시작하는 좌표(행,열)
-승객출발지(행,열) 승객도착지(행,열)
-'''
+n, m = map(int, input().split())
+
+logs = list(map(int, input().split()))
+
+low = 0
+high = max(logs)
+mid = 0
+get_log = 0
+ans = -1
+while low <= high:
+    mid = (high+low)//2
+    get_log = 0
+    for log in logs:
+        if log-mid > 0:
+            get_log += log-mid
+    # print(">>>", mid, get_log)
+    if get_log < m:
+        high = mid-1
+    elif get_log > m:
+        low = mid+1
+        ans = max(ans, mid)
+    elif get_log == m:
+        ans = max(ans, mid)
+        break
+
+print(ans)
