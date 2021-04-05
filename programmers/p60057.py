d8782dd2f34729dd84b4c5046027203940f845af
한정훈 <onooff@naver.com>
2021-04-05 22:46:26 +0900

    프로그래머스60057 문자열 압축

--- programmers/p60057.py
+++ programmers/p60057.py
@@ -1,7 +1,29 @@
 def solution(s):
-    answer = 0
-
-    for i in range(len(s)):
-        tmp = s
-
+    answer = len(s)
+    l = len(s)//2
+    for i in range(1, l+1):
+        # if len(s) % i == 0:
+        ns = ""
+        cnt = 0
+        pre = s[0:i]
+        # for j in range(0, len(s)+1, i):
+        j = 0
+        while(j < len(s)):
+            cut = s[j:j+i]
+            if pre == cut:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    ns += str(cnt)
+                ns += pre
+                cnt = 1
+                pre = cut
+            j += i
+        if cnt > 1:
+            ns += str(cnt)
+        ns += pre
+        cnt = 1
+        pre = cut
+        # print(i,ns,len(s))
+        answer = min(answer, len(ns))
     return answer
