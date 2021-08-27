48fb3364cdc9d5e1bc9586499bdf3d0fe21bac82
한정훈 <onooff@naver.com>
2021-08-28 02:20:27 +0900

    프로그래머스17678 [1차] 셔틀버스

--- programmers/p17678.py
+++ programmers/p17678.py
@@ -1,32 +1,42 @@
+'''
+n회 t간격 배차 m명까지 탈 수 있다
+'''
+
+
+def parse(input_val):
+    if type(input_val) == str:
+        h, m = map(int, input_val.split(":"))
+        return h*60+m
+    else:
+        m = str(input_val % 60).zfill(2)
+        h = str(input_val // 60).zfill(2)
+    return h+":"+m
+
+
 def solution(n, t, m, timetable):
+    times = list()
+    for tt in timetable:
+        times.append(parse(tt))
+    times.sort()
+    now = parse("09:00")
+    ans = 0
+    for i in range(n):
+        seats = m
+        if i == n-1:
+            while seats > 1 and len(times) > 0 and times[0] <= now:
+                times.pop(0)
+                seats -= 1
+            if len(times) == 0:
+                ans = now
+            else:
+                ans = min(now, times[0]-1)
+            break
 
-    timetable.sort()
-    hour = 9
-    min = 0
-
-    while n > 0:
-        bus_m = m
-        n -= 1
-        hour += (min // 60)
-        min %= 60
-
-        if n == 0:  # 막차일때:탈수있는 시간보기
-            while (int(timetable[0][:2]) < hour) or ((int(timetable[0][:2]) == hour) and (int(timetable[0][3:]) <= min)):
-                timetable.pop(0)
-                bus_m -= 1
-                if bus_m == 1:
-                    return (int(timetable[0][:2]) == hour) and (int(timetable[0][3:])
-            print(timetable)
-            pass
-        else:  # 막차아닐때:그냥보내기
-            while (int(timetable[0][:2]) < hour) or ((int(timetable[0][:2]) == hour) and (int(timetable[0][3:]) <= min)):
-                timetable.pop(0)
-                bus_m -= 1
-                if bus_m == 0:
-                    break
-
-        min += t
+        while seats > 0 and len(times) > 0 and times[0] <= now:
+            times.pop(0)
+            seats -= 1
+        now += t
+    return parse(ans)
 
 
-# print(solution(1, 1, 5, ["00:02", "00:01", "00:01", "00:01", "00:01"]))
-print(solution(1, 1, 5, ["08:00", "08:01", "08:02", "08:03"]))
+print(solution(2, 10, 2, ["09:10", "09:09", "08:00"]))
