8c90259cf44171def7026ac314d9deaa7a71eba5
한정훈 <onooff@naver.com>
2021-08-28 01:33:40 +0900

    프로그래머스60059 자물쇠와 열쇠

--- programmers/p60059.py
+++ programmers/p60059.py
@@ -1,18 +1,56 @@
+def turn(key_list, key_size):
+    for k in key_list:
+        k[0], k[1] = k[1], key_size-1-k[0]
+
+
 def solution(key, lock):
-    blank = []
-    for i in range(0, len(lock)):
-        for j in range(0, len(lock)):
-            if lock[i][j] == '0':
-                blank.append = (i, j)
+    key_size = len(key)
+    lock_size = len(lock)
 
-    for k in range(0, 4):
-        key_tmp = [""]*len(key)
-        for i in range(0, len(key)):
-            for j in range(0, len(key)):
-                key_tmp[j] = key[j][i]
-        key = key_tmp
+    key_list = list()
+    for i in range(key_size):
+        for j in range(key_size):
+            if key[i][j] == 1:
+                key_list.append([i, j])
 
-        for i in range(0, len(key)):
-            for j in range(0, len(key)):
+    lock_list = list()
+    for i in range(lock_size):
+        for j in range(lock_size):
+            if lock[i][j] == 0:
+                lock_list.append((i, j))
 
-    return False
+    if len(key_list) == 0 or len(key_list) < len(lock_list):
+        return False
+    if len(lock_list) == 0:
+        return True
+    first_lock = lock_list[0]
+    last_lock = lock_list[len(lock_list)-1]
+    lock_set = set(lock_list)
+    answer = False
+    for t in range(4):
+        for i in range(-key_size+1+first_lock[0], last_lock[0]+1):
+            for j in range(-key_size+1+first_lock[1], last_lock[1]+1):
+                cnt = 0
+                for k in key_list:
+                    chk = (k[0]+i, k[1]+j)
+                    if chk in lock_set:
+                        cnt += 1
+                        if cnt == len(lock_set):
+                            answer = True
+                            break
+                    elif 0 <= chk[0] < lock_size and 0 <= chk[1] < lock_size:
+                        break
+                if answer:
+                    break
+            if answer:
+                break
+        if answer:
+            break
+
+        if t < 3:
+            turn(key_list, key_size)
+    return answer
+
+
+print(solution(	[[1, 0], [0, 0]],
+                [[0, 1, 1], [1, 1, 1], [1, 1, 0]]))
