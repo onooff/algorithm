686e48ed1bd4d19e56171b68a0105354c3a1a5c9
한정훈 <onooff@naver.com>
2022-07-01 21:16:58 +0900

    백준1308 D-Day

--- boj/bj1308.py
+++ boj/bj1308.py
@@ -1,8 +1,14 @@
 import datetime
 start = list(map(int, input().split()))
 end = list(map(int, input().split()))
-if end[0]-start[0] >= 1000 and end[1] >= start[1] and end[2] >= start[2]:
-    print('gg')
+ans = 'gg'
+if end[0]-start[0] > 1000:
+    pass
+elif end[0]-start[0] == 1000 and end[1] > start[1]:
+    pass
+elif end[0]-start[0] == 1000 and end[1] == start[1] and end[2] >= start[2]:
+    pass
 else:
-    print('D-', (datetime.date(end[0], end[1], end[2]) -
-                 datetime.date(start[0], start[1], start[2])).days, sep='')
+    ans = 'D-'+str((datetime.date(end[0], end[1], end[2]) -
+                    datetime.date(start[0], start[1], start[2])).days)
+print(ans)
