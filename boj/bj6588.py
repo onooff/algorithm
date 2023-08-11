b86948a3fb3b64bf8ed5c469d9863e8c71a9c708
한정훈 <onooff@naver.com>
2023-08-11 15:28:03 +0900

    백준6588 골드바흐의 추측

--- boj/bj6588.py
+++ boj/bj6588.py
@@ -1,26 +1,30 @@
-# import time
-# s = time.time()
+import sys
+
+input = sys.stdin.readline
+
 limit = 1000000
-l = [True]*(limit+1)
+l = [True] * (limit + 1)
 m = int(limit**0.5)
 primes = list()
 for i in range(2, m):
     if l[i]:
-        for k in range(i+i, len(l), i):
+        for k in range(i + i, len(l), i):
             l[k] = False
 for i in range(2, len(l)):
     if l[i]:
         primes.append(i)
-# e = time.time()
-# print(e-s)
+
+ans = list()
 while True:
     n = int(input())
     if n == 0:
         break
     for p in primes:
-        if l[n-p]:
-            print(n, '=', p, '+', n-p)
+        if l[n - p]:
+            ans.append(f"{n} = {p} + {n-p}")
             break
         elif n <= p:
-            print("Goldbach's conjecture is wrong.")
+            ans.append("Goldbach's conjecture is wrong.")
             break
+
+print("\n".join(ans))
