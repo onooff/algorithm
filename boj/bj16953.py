5ed599431240e3e431b544ceecf6656ff5211d50
JeongHun Han <onooff@naver.com>
2025-07-28 00:43:51 +0900

    백준16953 A → B

---
+++ boj/bj16953.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+
+
+def go(b, a, cnt):
+    if b == a:
+        return cnt
+    if b % 10 == 1:
+        return go((b - 1) // 10, a, cnt + 1)
+    if b > 0 and b % 2 == 0:
+        return go(b // 2, a, cnt + 1)
+    return -1
+
+
+print(go(b, a, 1))
