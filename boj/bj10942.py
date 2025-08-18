e96b09c8af2a4f0909974311508055376e81b8f5
JeongHun Han <onooff@naver.com>
2025-08-19 01:18:29 +0900

    백준10942 팰린드롬?

--- boj/bj10942.py
+++ boj/bj10942.py
@@ -1,35 +1,27 @@
 import sys
+
 inputs = sys.stdin.readlines()
 n, m = int(inputs[0]), int(inputs[2])
 l = list(map(int, inputs[1].split()))
-pd = dict()
+memo = {i: {i} for i in range(n)}
 for i in range(n):
-    s = i
-    e = i
-    while s >= 0 and e < n:
-        if l[s] == l[e]:
-            pd.setdefault(s, set())
-            pd[s].add(e)
-            s -= 1
-            e += 1
-        else:
-            break
-    if i+1 < n and l[i] == l[i+1]:
-        s = i
-        e = i+1
-        while s >= 0 and e < n:
-            if l[s] == l[e]:
-                pd.setdefault(s, set())
-                pd[s].add(e)
-                s -= 1
-                e += 1
-            else:
-                break
-
-for i in range(3, m+3):
-    s, e = map(int, inputs[i].split())
-    s, e = s-1, e-1
-    if s in pd and e in pd[s]:
+    left = i - 1
+    right = i + 1
+    while left >= 0 and right < n and l[left] == l[right]:
+        memo[left].add(right)
+        left -= 1
+        right += 1
+    if i + 1 < n and l[i] == l[i + 1]:
+        memo[i].add(i + 1)
+        left = i - 1
+        right = i + 2
+        while left >= 0 and right < n and l[left] == l[right]:
+            memo[left].add(right)
+            left -= 1
+            right += 1
+for i in range(3, len(inputs)):
+    s, e = map(lambda x: int(x) - 1, inputs[i].split())
+    if s in memo and e in memo[s]:
         print(1)
     else:
         print(0)
